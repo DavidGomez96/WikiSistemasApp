edada2ca45de16c8f6140ad829fa8debf90d6d66
david91921 <david.ardilagomez@gmail.com>
2024-05-30 00:00:44 -0500

    commit del cors

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 import express from 'express';
+import cors from 'cors'
 import materiaRouter from './routes/materiasRoute.js';
 import recursoRouter from './routes/recursosRoute.js';
 import usuarioRouter from './routes/usuariosRoute.js';
 import semestreRoute from './routes/semestreRoute.js';
 
 
-
 const app = express();
 const PORT = 8080;
 
 app.use(express.json()); // Para manejar las solicitudes con cuerpo en formato JSON
+app.use(cors())
 
 // Usar el router de materias
 app.use('/materias', materiaRouter);
